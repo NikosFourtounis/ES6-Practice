226db217054a8afd44dafdfd8350cb4a238538f2
N Fourtounis <nickfortune@windowslive.com>
2018-12-03 11:28:29 +0200

    React app

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
-import Entity from './entity';
+import React, {Component } from 'react';
+import ReactDOM from 'react-dom';
 
-class Hobbit extends Entity {
-    constructor(name, height){
-        super(name,height);
+class App extends Component {
+    render() {
+        return (
+            <div>React JS and JSX in action</div>
+        )
     }
 }
 
-let Frodo = new Hobbit("Frodo Baggins", 4.5);
-console.greet();
+ReactDOM.render(<App></App>, document.getElementById('root'));
